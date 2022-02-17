fa057a73596ff886e6a68f79b8080ae8ea8a0044
Paulo Ferreira <paulo.ferreira@ditech.com.br>
2022-02-17 12:41:02 -0300

    Fix

--- src/pages/qrcode2.tsx
+++ src/pages/qrcode2.tsx
@@ -6,13 +6,8 @@ import imagemQrCode from '../../public/imagem_qrcode.png'
 export default function QrCodePage() {
   const router = useRouter()
   return (
-    <div className={`
-      h-screen bg-black flex justify-center items-center
-    `}>
-      <div className={`
-        bg-[url('../../public/tag.jpeg')] h-screen w-1/2
-        bg-cover flex justify-center items-center
-    `}>{/*<QrCode name={router.query.name} email={router.query.email} company={router.query.company} phone={router.query.phone} instagram={router.query.instagram} />*/}</div>
+    <div className={``}>
+      <QrCode name={router.query.name} email={router.query.email} company={router.query.company} phone={router.query.phone} instagram={router.query.instagram} />
     </div>
   )
 }
