1363db9bb69cd28864f7e3c3f71da81725b43dea
Paulo Ferreira <paulo.ferreira@ditech.com.br>
2022-02-17 13:15:21 -0300

    Fix

--- src/pages/qrcode2.tsx
+++ src/pages/qrcode2.tsx
@@ -1,14 +1,14 @@
 import QrCode from "../components/QrCode"
 import Image from 'next/image'
 import { useRouter } from 'next/router'
-import tag from '../../public/tag_v1.jpeg'
+import tag from '../../public/tag_v2.jpeg'
 
 export default function QrCodePage() {
   const router = useRouter()
   return (
     <div className={``}>
       <Image src={ tag } alt="match icon" />
-      <div className="absolute left-14 top-11"><QrCode name={router.query.name} email={router.query.email} company={router.query.company} phone={router.query.phone} instagram={router.query.instagram} /></div>
+      <div className="absolute left-44 top-24"><QrCode name={router.query.name} email={router.query.email} company={router.query.company} phone={router.query.phone} instagram={router.query.instagram} /></div>
     </div>
   )
 }
