a5a6a262fe62bda337e28f0c964f7bbbae3e5447
Paulo Ferreira <jr@live.at>
2022-04-03 18:52:31 -0300

    bug

--- src/pages/qrcode2.tsx
+++ src/pages/qrcode2.tsx
@@ -14,9 +14,9 @@ interface IProps {
 export default class QrCodePage extends React.Component<IProps> {
   render() {
     return (
-      <div className='yourClassName'>
-        <Image src={ tag } alt="match icon" />
-        <div className="absolute left-36 top-20">
+      <div className='yourClassName2'>
+        <Image src={ tag } alt="match icon" width={800} height={800}/>
+        <div className="absolute left-44 top-24">
           <QrCode name={this.props.name} email={this.props.email} company={this.props.company} phone={this.props.phone} instagram={this.props.instagram} />
         </div>
       </div>
