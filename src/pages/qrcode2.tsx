6ca953e53280acef6e061a9d2fd9088174b0f3a9
Paulo Junior <jr@live.at>
2023-10-12 15:22:03 -0300

    Fix qrcode position

--- src/pages/qrcode2.tsx
+++ src/pages/qrcode2.tsx
@@ -14,15 +14,11 @@ interface IProps {
 export default class QrCodePage extends React.Component<IProps> {
   render() {
     return (
-      <div className={`
-        yourClassName
-      `} >
-        <Image src={ tag } alt="match icon" width={710} height={710} />
-        { 
-        <div className="absolute left-[146] top-[105]">
+      <div style={{ position: 'relative', display: 'inline-block' }} className={` yourClassName `} >
+        <Image src={ tag } alt="Imagem 1" style={{ width: '100%', height: 'auto' }} />
+        <div style={{ position: 'absolute', top: '43%', left: '50%', transform: 'translate(-50%, -50%)' }}>
           <QrCode name={this.props.name} email={this.props.email} company={this.props.company} phone={this.props.phone} instagram={this.props.instagram} />
-    </div>
-     }
+        </div>
       </div>
     )
   }
