e1f516998dd506780b406717559f7933d49bd888
Paulo Junior <jr@live.at>
2023-10-18 13:30:29 -0300

    Teste

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import Form from "../components/Form";
 import Image from 'next/image'
 import matchWork from '../../public/match_work.png'
 import imgForm from '../../public/imagem_form.png'
@@ -10,10 +9,6 @@ export default function Home() {
           <div className={`p-7`}>
             <Image src={matchWork} alt="match icon" width={348} height={153}  />
           </div>
-        </div>
-        <div className={`w-screen flex`}>
-          <div className="w-2/3 flex pr-16"><Form /></div>
-          <div className="w-1/3"><Image src={imgForm } width={500} height={500} alt="match icon"/></div>
         </div>
     </div>
   )
